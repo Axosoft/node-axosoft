c164795b821795a66cd544076f3b06b819a7df2c
James Birkett <james.birkett@footworksolutions.co.uk>
2015-12-18 10:08:33 +0000

    Partial Commit - moved main script to /lib

--- index.js
+++ index.js
@@ -1,67 +1,22 @@
-var _ = require('lodash');
-var rest = require('restler');
+var moment = require('moment');
 
-var ls_base_url = 'https://footworksolutions.axosoft.com/';
-
-var credentials = {
-	grant_type: 'password',
+var node_axosoft = require('./lib/node-axosoft')('https://footworksolutions.axosoft.com', {
 	username: 'api',
 	password: 'bridgehouse123',
 	client_id: '9ad13c32-187f-4b53-a45e-218ebcbf309d',
 	client_secret: 'XL6vs6424I_1kLrGoMbB5RD00PTgNf5AvOAqSBNHDwSUhD61qkMxrDDjAC-bdT-3yiB9-vyr0u8CinB3WsbfLFaKsm9PtGqBHfrZ'
-};
-
-var required_credentials = ['grant_type','username','password','client_id','client_secret'];
-
-//OAuth to get our Podio access token
-rest.get(ls_base_url + 'api/oauth2/token', {data: credentials}).on('complete', function (data, response) {
-		console.log(data);
-	if (data.access_token){
-
-		rest.get(ls_base_url + 'api/v5/work_logs', {
-			data: {
-				page:1,
-				page_size: 10,
-				start_date: '2015-12-10',
-				end_date: '2015-12-15'
-			},
-			headers: {'Authorization': 'Bearer ' + data.access_token}
-		}).on('complete', function (data, response) {
-			console.log(data);
-		});
-		// (Re)set current access details
-	}
-	else {
-
-		// Return an informative error object
-		// callback({statusCode: response.statusCode, responseRaw: response.rawEncoded});
-	}
 });
 
-function _test_required_credentials () {
 
-}
+node_axosoft.getWorkLogs({
+	page:1,
+	page_size: 1,
+	start_date: moment().subtract(1, 'week').format('YYYY-MM-DD'),
+	end_date:  moment().format('YYYY-MM-DD')
+}, function (err, data) {
+	if (err) {
+		process.exit(err);
+	}
 
-
-function NodeAxosoft (credentials) {
-	var node_axosoft = {
-		VERSION: '1.0.0'
-	};
-
-
-	if (!_.has(credentials, 'app_id')) {
-		throw new Error('You must specify an app_id');
-	}
-	if (!_.has(credentials, 'app_token')) {
-		throw new Error('You must specify an app_token');
-	}
-	if (!_.has(credentials, 'client_id')) {
-		throw new Error('You must specify a client_id');
-	}
-	if (!_.has(credentials, 'client_secret')) {
-		throw new Error('You must specify a client_secret');
-	}
-	if (!_.has(credentials, 'client_secret')) {
-		throw new Error('You must specify a client_secret');
-	}
-}
+	console.log(data);
+});
